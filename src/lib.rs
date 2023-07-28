505937b0f149619623a90d9047914736e83e38f4
Skyf0l <tom.rorato@epitech.eu>
2023-07-28 18:09:37 +0200

    feat: trial mul algo

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,32 @@
 #![warn(missing_docs)]
 
 use rug::Integer;
+mod trial_mul;
 
-/// Compute the discrete logarithm of `h` in base `g` modulo `n`.
-/// Returns the smallest non-negative integer `x` where `g**x = h (mod n)`.
-pub fn discrete_log(_n: &Integer, _a: &Integer, _b: &Integer) -> Integer {
-    todo!()
+pub use trial_mul::discrete_log_trial_mul;
+use trial_mul::discrete_log_trial_mul_with_order;
+
+/// Discrete logarithm error
+#[derive(thiserror::Error, Debug, Clone, PartialEq, Eq)]
+pub enum Error {
+    /// Log does not exist
+    #[error("Log does not exist")]
+    LogDoesNotExist,
+}
+
+/// Compute the discrete logarithm of `h` in base `g` modulo `n` (smallest non-negative integer `x` where `g**x = h (mod n)`).
+pub fn discrete_log(n: &Integer, a: &Integer, b: &Integer) -> Result<Integer, Error> {
+    discrete_log_trial_mul_with_order(n, a, b, &None)
+}
+
+/// Compute the discrete logarithm of `h` in base `g` modulo `n` (smallest non-negative integer `x` where `g**x = h (mod n)`).
+///
+/// If the order of the group is known, it can be passed as `order` to speed up the computation.
+pub fn discrete_log_with_order(
+    n: &Integer,
+    a: &Integer,
+    b: &Integer,
+    order: &Option<Integer>,
+) -> Result<Integer, Error> {
+    discrete_log_trial_mul_with_order(n, a, b, order)
 }
---
+++ src/trial_mul.rs
@@ -0,0 +1,73 @@
+use rug::Integer;
+
+use crate::Error;
+
+/// Trial multiplication algorithm for computing the discrete logarithm of `h` in base `g` modulo `n` (smallest non-negative integer `x` where `g**x = h (mod n)`)
+///
+/// The algorithm finds the discrete logarithm using exhaustive search.
+/// This naive method is used as fallback algorithm of ``discrete_log`` when the group order is very small.
+pub fn discrete_log_trial_mul(n: &Integer, a: &Integer, b: &Integer) -> Result<Integer, Error> {
+    discrete_log_trial_mul_with_order(n, a, b, &None)
+}
+
+/// Trial multiplication algorithm for computing the discrete logarithm of `h` in base `g` modulo `n` (smallest non-negative integer `x` where `g**x = h (mod n)`)
+///
+/// The algorithm finds the discrete logarithm using exhaustive search.
+/// This naive method is used as fallback algorithm of ``discrete_log`` when the group order is very small.
+///
+/// If the order of the group is known, it can be passed as `order` to speed up the computation.
+pub fn discrete_log_trial_mul_with_order(
+    n: &Integer,
+    a: &Integer,
+    b: &Integer,
+    order: &Option<Integer>,
+) -> Result<Integer, Error> {
+    let a = a.clone() % n;
+    let b = b.clone() % n;
+    let order = match order.as_ref() {
+        Some(order) => order,
+        None => n,
+    };
+
+    let mut x = Integer::from(1);
+    let mut i = 0;
+    loop {
+        if x == a {
+            return Ok(Integer::from(i));
+        }
+        x = x * &b % n;
+
+        i += 1;
+        if i == *order {
+            break;
+        }
+    }
+    Err(Error::LogDoesNotExist)
+}
+
+#[cfg(test)]
+mod tests {
+    use rug::ops::Pow;
+
+    use super::*;
+
+    #[test]
+    fn trial_mul() {
+        assert_eq!(
+            discrete_log_trial_mul(&587.into(), &(Integer::from(2).pow(7)), &2.into()).unwrap(),
+            7
+        );
+        assert_eq!(
+            discrete_log_trial_mul(&941.into(), &(Integer::from(7).pow(18)), &7.into()).unwrap(),
+            18
+        );
+        assert_eq!(
+            discrete_log_trial_mul(&389.into(), &(Integer::from(3).pow(81)), &3.into()).unwrap(),
+            81
+        );
+        assert_eq!(
+            discrete_log_trial_mul(&191.into(), &(Integer::from(19).pow(123)), &19.into()).unwrap(),
+            123
+        );
+    }
+}
